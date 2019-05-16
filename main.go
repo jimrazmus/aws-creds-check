f68d017aa5028555b9016e224f929a2d95ae0181
Jim Razmus II <jim.razmus@code42.com>
2019-05-16 15:49:16 -0500

    Add files

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Print("No .env file to load, skipping.")
+	}
+
+	cfg := aws.NewConfig()
+	cfg.WithCredentialsChainVerboseErrors(true)
+
+	session := session.Must(session.NewSession(cfg))
+	aws_sts := sts.New(session)
+
+	input := &sts.GetCallerIdentityInput{}
+	result, err := aws_sts.GetCallerIdentity(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				log.Fatal(aerr.Error())
+			}
+		} else {
+			log.Fatal(err.Error())
+		}
+	} else {
+		log.Print(result)
+	}
+
+}
